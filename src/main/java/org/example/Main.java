5cdd76316f94fd91acfb27e7daec3ef905f0e6a7
Artem Ostapchuk <artem2050art@gmail.com>
2024-01-06 22:27:30 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Calculator.sum(13, 8));
+        System.out.println(Calculator.sum(3,9));
+    }
+}
